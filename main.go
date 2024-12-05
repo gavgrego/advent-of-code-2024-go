cc3b0eca48bf1e1c267dc1c7a5ca2347c2c10d30
Gavin Gregory <gavgrego@gmail.com>
2024-12-05 05:13:44 -0800

    polish by removing uncessary comparison, it's fine if values are the same

--- main.go
+++ main.go
@@ -10,13 +10,9 @@ import (
 func main() {
 	var total int
 	for i := 0; i < len(config.FirstList); i++ {
-		if config.FirstList[i] != config.SecondList[i] {
-			difference := math.Abs(float64(config.FirstList[i] - config.SecondList[i]))
-			total += int(difference)
-		}
+		difference := math.Abs(float64(config.FirstList[i] - config.SecondList[i]))
+		total += int(difference)
 	}
 
 	fmt.Println("Total", total)
 }
-
-
